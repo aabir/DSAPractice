ef9e59d6201be5bf5525f9d867d0d677c9c12300
aabir <shblnmn@gmail.com>
2023-01-09 00:15:01 +0800

    LT 1281

---
+++ SubtractTheProductandSumOfDigitsofanInteger/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SubtractTheProductandSumOfDigitsofanInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.SubtractProductAndSum(234));
+        }
+    }
+
+    public class Solution
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            List<int> listOfDigit = new List<int>();
+
+            while (n > 0)
+            {
+                listOfDigit.Add(n % 10);
+                n = n / 10;
+            }
+            int product = 1, sum = 0;
+            for(int i = 0; i < listOfDigit.Count; i++)
+            {
+                product *= listOfDigit[i];
+                sum += listOfDigit[i];
+            }
+            return product - sum;
+        }
+    }
+}
