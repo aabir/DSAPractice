81b2e30a1391c3d189dde095b9f998df06856c3c
aabir <shblnmn@gmail.com>
2023-01-15 18:33:01 +0800

    LC 1389

---
+++ CreateTargetArrayintheGivenOrder/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CreateTargetArrayintheGivenOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0, 1, 2, 3, 4 };
+            int[] index = new int[] { 0, 1, 2, 2, 1 };
+
+            var targetAry = new Solution().CreateTargetArray(nums, index);
+            Console.WriteLine(string.Join(",", targetAry));
+        }
+    }
+    public class Solution
+    {
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            List<int> target = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                target.Insert(index[i], nums[i]);
+            }
+            return target.ToArray();
+        }
+    }
+}
