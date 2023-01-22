e9f0501d05da98691e949ae01bb9298c5858e3a0
aabir <shblnmn@gmail.com>
2023-01-22 12:15:01 +0800

    LC 2194

---
+++ CellsinaRangeonanExcelSheet/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CellsinaRangeonanExcelSheet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cellsInRange = new Solution().CellsInRange("K1:L2");
+            Console.WriteLine(String.Join(",", cellsInRange));
+        }
+
+        public class Solution
+        {
+            public IList<string> CellsInRange(string s)
+            {
+                List<string> result = new List<string>();
+                for (char ch = s[0]; ch <= s[3]; ch++)
+                {
+                    for (char ch2 = s[1]; ch2 <= s[4]; ch2++)
+                    {
+                        char[] chars = { ch, ch2 };
+                        result.Add(new string(chars));
+                    }
+                }
+                return result;
+            }
+        }
+
+    }
+}
