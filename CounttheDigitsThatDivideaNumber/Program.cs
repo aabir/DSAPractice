0639278168c4c0f4d42e6868f8fe8dd858a46db3
aabir <shblnmn@gmail.com>
2023-02-13 15:59:21 +0800

    LT 2325

---
+++ CounttheDigitsThatDivideaNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CounttheDigitsThatDivideaNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().CountDigits(121);
+            Console.WriteLine(solution);
+        }
+    }
+
+    public class Solution
+    {
+        public int CountDigits(int num)
+        {
+            int result = 0;
+            int temNum = num;
+
+            while (temNum > 0)
+            {
+                int k = temNum % 10;
+                temNum /= 10;
+
+                if(num % k == 0) result++;
+            }
+            return result;
+           
+        }
+    }
+
+}
