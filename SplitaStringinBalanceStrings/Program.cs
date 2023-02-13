345a6c01088f44c432c6c70b7a17d74089ec7e52
aabir <shblnmn@gmail.com>
2023-02-13 13:21:08 +0800

    LT 1221

---
+++ SplitaStringinBalanceStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SplitaStringinBalanceStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutions = new Solution().BalanceStringSplit("RLRRRLLRLL");
+            Console.WriteLine(solutions.ToString());
+        }
+    }
+
+    public class Solution
+    {
+        public int BalanceStringSplit(string s)
+        {
+            int counter = 0;
+            int points = 0;
+
+            foreach(var chara in s)
+            {
+                if (chara.ToString().Equals("R")) counter++;
+                else counter--;
+
+                if(counter == 0)
+                    points++;
+            }
+            return points;
+        }
+    }
+}
