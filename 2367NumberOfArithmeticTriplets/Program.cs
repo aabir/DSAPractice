709b6bdd0e7d4312f46e7e21ec52f4496da03dae
aabir <shblnmn@gmail.com>
2023-02-19 13:09:40 +0800

    LT 2367

---
+++ 2367NumberOfArithmeticTriplets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace _2367NumberOfArithmeticTriplets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 4, 6, 7, 10 };
+            var solution = new Solution().ArithmeticTriplets(nums, 3);
+            Console.WriteLine(solution);
+        }
+    }
+
+    public class Solution
+    {
+        public int ArithmeticTriplets(int[] nums, int diff)
+        {
+            int result = 0;
+            HashSet<int> hashSet = new HashSet<int>();
+
+            foreach(int num in nums)
+            {
+                if(hashSet.Contains(num - diff) && hashSet.Contains(num - 2 * diff))
+                {
+                    result++;
+                }
+                hashSet.Add(num);
+            }
+            return result;
+        }
+    }
+}
