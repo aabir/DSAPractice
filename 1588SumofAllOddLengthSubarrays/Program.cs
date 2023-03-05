c1f1e3689027bca09e2a12a6ba38c632c025e537
aabir <shblnmn@gmail.com>
2023-03-05 20:47:17 +0800

    LT 1588

---
+++ 1588SumofAllOddLengthSubarrays/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _1588SumofAllOddLengthSubarrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2 };
+            var solution = new Solution().SumOddLengthSubarrays(arr);
+            Console.WriteLine(solution);
+        }
+    }
+    public class Solution
+    {
+        public int SumOddLengthSubarrays(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 1; i <= arr.Length; i += 2)
+            {
+                for(int j = 0; j + i - 1 < arr.Length; j++)
+                {
+                    for(int k = j; k < j + i; k++)
+                    {
+                        sum += arr[k];
+                    }
+                }
+            }
+            return sum;
+        }
+
+        
+    }
+    
+}
