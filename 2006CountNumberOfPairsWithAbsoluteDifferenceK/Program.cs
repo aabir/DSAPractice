e56834b9f4ef651b21ca0b949cca2419dd09bbeb
aabir <shblnmn@gmail.com>
2023-04-01 16:17:55 +0800

    LT 2006

---
+++ 2006CountNumberOfPairsWithAbsoluteDifferenceK/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _2006CountNumberOfPairsWithAbsoluteDifferenceK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().CountKDifference(new int []{ 3, 2, 1, 5, 4 }, 2);
+            Console.WriteLine(solution);
+        }
+
+        public class Solution
+        {
+            public int CountKDifference(int[] nums, int k)
+            {
+                if (nums.Length == 0) return 0;
+
+                int count = 0;
+                for(int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        if (nums[i] - nums[j] == k) count++;
+                    }
+                    
+                }
+                return count;
+            }
+        }
+    }
+}
