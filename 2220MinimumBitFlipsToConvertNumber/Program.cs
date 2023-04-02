9567e7846a0a10acfe3baffdc888192c758d49f4
aabir <shblnmn@gmail.com>
2023-04-02 11:26:42 +0800

    LT 2220

---
+++ 2220MinimumBitFlipsToConvertNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2220MinimumBitFlipsToConvertNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().MinBitFlips(10, 7);
+            Console.WriteLine(solution);
+        }
+
+        public class Solution
+        {
+            public int MinBitFlips(int start, int goal)
+            {
+                int result = start ^ goal;
+                int output = 0;
+                while (result != 0) 
+                {
+                    if ((result & 1) == 1) { output++; }
+                    result = result >> 1;
+                }
+                return output;
+            }
+        }
+    }
+}
