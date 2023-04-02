58296e60b28342b4df32df994a2f9ff555715157
aabir <shblnmn@gmail.com>
2023-04-02 11:03:36 +0800

    LT 804

---
+++ 804UniqueMorseCodeWords/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace _804UniqueMorseCodeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().UniqueMorseRepresentations(new string[] { "gin", "zen", "gig", "msg" });
+            Console.WriteLine(solution);
+        }
+
+        public class Solution
+        {
+            public int UniqueMorseRepresentations(string[] words)
+            {
+                string[] codes = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+                StringBuilder sb = new StringBuilder();
+
+                HashSet<string> set = new HashSet<string>();
+
+                
+                foreach (var word in words) 
+                {
+                    sb = new();
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        int index = word[i] - 'a';
+                        sb.Append(codes[index]);
+                    }
+                    set.Add(sb.ToString());
+                }
+
+                return set.Count;
+                
+            }
+        }
+    }
+}
