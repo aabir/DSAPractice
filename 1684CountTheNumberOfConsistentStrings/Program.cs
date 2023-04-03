e8b14854cb460b35f955af29555f9e81f74fa69e
aabir <shblnmn@gmail.com>
2023-04-03 14:35:45 +0800

    LT 1684

---
+++ 1684CountTheNumberOfConsistentStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _1684CountTheNumberOfConsistentStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().CountConsistentStrings("ab", new string[] { "ad", "bd", "aaab", "baa", "badab" });
+            Console.WriteLine(solution);
+        }
+
+        public class Solution
+        {
+            public int CountConsistentStrings(string allowed, string[] words)
+            {
+                int num = words.Length;
+
+                int total = 0;
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    foreach (var c in words[i].ToCharArray()) 
+                    {
+                        if (!allowed.Contains(c))
+                        {
+                            total++;
+                            break;
+                        }
+                    }
+                }
+                return num - total;
+            }
+        }
+    }
+}
