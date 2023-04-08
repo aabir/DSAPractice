14363acd81285180bc55329781e469c7660d60e4
aabir <shblnmn@gmail.com>
2023-04-08 18:39:11 +0800

    LT 1614

---
+++ 1614MaximumNestingDepthoftheParentheses/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _1614MaximumNestingDepthoftheParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutions = new Solution().MaxDepth("(1)+((2))+(((3)))");
+            Console.WriteLine(solutions);
+        }
+
+        public class Solution
+        {
+            public int MaxDepth(string s)
+            {
+                int count = 0;
+                int depth = 0;  
+                   
+                for(int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(')
+                    {
+                        count++;
+                        if (count > depth)
+                            depth = count;
+                    }
+                    else if (s[i] == ')') { count--; }
+                }
+                return depth;
+            }
+        }
+    }
+}
