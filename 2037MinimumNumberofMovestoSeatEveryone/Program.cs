d669ee0fb5d753b4fd2eb0e86a5c03a1b3d94036
aabir <shblnmn@gmail.com>
2023-04-09 13:29:06 +0800

    LT 2037, 2315

---
+++ 2037MinimumNumberofMovestoSeatEveryone/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _2037MinimumNumberofMovestoSeatEveryone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutions = new Solution().MinMovesToSeat(new int[] { 3, 1, 5 }, new int[] { 2, 7, 4 });
+            Console.WriteLine(solutions);
+        }
+
+        public class Solution
+        {
+            public int MinMovesToSeat(int[] seats, int[] students)
+            {
+                Array.Sort(seats);
+                Array.Sort(students);
+
+                int count = 0;
+
+                for(int i = 0; i < seats.Length; i++)
+                {
+                    count += Math.Abs(seats[i] - students[i]);
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ 2315CountAsterisks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2315CountAsterisks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solutions = new Solution().CountAsterisks("l|*e*et|c**o|*de|");
+            Console.WriteLine(solutions);
+        }
+
+        public class Solution
+        {
+            public int CountAsterisks(string s)
+            {
+                var bar = s.Split('|');
+                int count = 0;
+
+                for (int i = 0; i < bar.Length; i+=2)
+                {
+                    count += bar[i].Count(x => x == '*');
+                }
+                return count;
+            }
+        }
+    }
+}
