855d0d4916464ed13b88fe32e09d36d016fc8399
aabir <shblnmn@gmail.com>
2023-04-16 00:06:42 +0800

    LT Reverse Words in a String III

---
+++ 557ReverseWordsinaString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace _557ReverseWordsinaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().ReverseWords("Let's take LeetCode contest");
+            Console.WriteLine(solution);
+        }
+
+        public class Solution
+        {
+            public string ReverseWords(string s)
+            {
+                var ary = s.Split(' ');
+
+                for (int i = 0 ; i < ary.Length; i++)
+                {
+                    var word = ary[i].ToArray();
+                    var sb = new StringBuilder();
+                    for (int j = word.Length - 1; j >= 0; j--) 
+                    {
+                        sb.Append(word[j]);
+                    }
+                    ary[i] = sb.ToString();
+                }
+                return string.Join(" ", ary);
+            }
+        }
+    }
+}
