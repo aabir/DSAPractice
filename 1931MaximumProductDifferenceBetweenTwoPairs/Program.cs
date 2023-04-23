3b64de780fcdffab4b6a4bd959085a7963495d96
aabir <shblnmn@gmail.com>
2023-04-23 12:43:26 +0800

    LT 1931

---
+++ 1931MaximumProductDifferenceBetweenTwoPairs/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _1931MaximumProductDifferenceBetweenTwoPairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution().MaxProductDifference(new int[] { 5, 6, 2, 7, 4 });
+            Console.WriteLine(solution);
+        }
+
+        public class Solution
+        {
+            public int MaxProductDifference(int[] nums)
+            {
+                Array.Sort(nums);
+                return (nums[nums.Length - 1] * nums[nums.Length - 2]) - (nums[0] * nums[1]);
+                
+            }
+        }
+    }
+}
