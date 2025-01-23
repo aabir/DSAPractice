1f61d755058a8f8eceb00079aa592541da574d97
aabir <shblnmn@gmail.com>
2025-01-23 11:07:16 -0800

    01Two sum

---
+++ 01TwoSum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15};
+            int target = 9;
+            var solution = new Solution().TwoSum(nums, target);
+
+            Console.WriteLine($"{solution[0]} - {solution[1]}" );
+        }
+
+        public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                var numToIndex = new Dictionary<int, int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int complement = target - nums[i];
+                    if (numToIndex.ContainsKey(complement))
+                    {
+                        return [numToIndex[target - nums[i]], i];
+                    }
+                    numToIndex[nums[i]] = i;
+                }
+                return new int[0];
+            }
+        }
+    }
+}
