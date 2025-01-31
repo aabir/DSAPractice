b58a5db457ca36aec0caeea9d8a8c0ee0c9eba0b
aabir <shblnmn@gmail.com>
2025-01-31 11:47:41 -0800

    LC - 20. Valid Parentheses

---
+++ 20ValidParentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _20ValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsValid("()"));      // True
+            Console.WriteLine(solution.IsValid("()[]{}"));
+        }
+
+        public class Solution
+        {
+            public bool IsValid(string s)
+            {
+                Stack<char> chars = new Stack<char>();
+
+                Dictionary<char, char> map = new Dictionary<char, char>
+                {
+                    { ')', '(' },
+                    { '}', '{' },
+                    { ']', '[' }
+                };
+
+                foreach (char c in s)
+                {
+                    if (map.ContainsKey(c))
+                    {
+                        if (chars.Count == 0 || chars.Pop() != map[c])
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        chars.Push(c);
+                    }
+                }
+
+                return chars.Count == 0;
+            }
+        }
+    }
+}
